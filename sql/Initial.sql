22497ab2012f35da7328313fe7a8b51bab2449a4
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-09-01 00:20:12 +0100

    Add separate database container

--- sql/Initial.sql
+++ sql/Initial.sql
@@ -1,7 +1,20 @@
+CREATE DATABASE [Todobernetes]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Todobernetes', FILENAME = N'/var/opt/mssql/data/Todobernetes.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Todobernetes_log', FILENAME = N'/var/opt/mssql/data/Todobernetes_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+GO
+
+USE [Todobernetes]
+GO
+
 CREATE TABLE [TodoItems] (
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Name] NVARCHAR(MAX) NOT NULL,
 	[Completed] BIT NOT NULL
-);
+)
+GO
 
-ALTER TABLE [TodoItems] ADD  PRIMARY KEY (Id);
+ALTER TABLE [TodoItems] ADD  PRIMARY KEY (Id)
+GO
